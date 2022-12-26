c05d0921fa5caaf171595127a860f59410742109
Ali Akram <akramsystems@gmail.com>
2022-12-25 20:52:55 -0500

    persist state of the todoList when closing and opening extension

--- todo.js
+++ todo.js
@@ -1,66 +1,130 @@
-// todo.js
+// Initialize the todo list array
 
-// Keep track of the todo list items
+// what does the following line do ?
 let todoList = [];
 
-// Add an item to the todo list
+
+chrome.storage.local.get(['todoList'], function(result) {
+  if (result.todoList) {
+    todoList = result.todoList;
+  }
+  renderTodoList();
+});
+
+chrome.storage.onChanged.addListener(function(changes) {
+  if ('todoList' in changes) {
+    todoList = changes.todoList.newValue;
+    console.log('Todo list reloaded from storage');
+    renderTodoList();
+  }
+});
+
+// Add a new item to the todo list
 function addTodo(text) {
   todoList.push({
     text: text,
     completed: false
   });
-  updateProgressBar();
+  saveTodoList();
 }
 
 // Remove an item from the todo list
 function removeTodo(index) {
   todoList.splice(index, 1);
-  updateProgressBar();
+  saveTodoList();
 }
 
-// Update the progress bar showing how many items are completed and remaining
-function updateProgressBar() {
-  let completedCount = todoList.filter(todo => todo.completed).length;
-  let remainingCount = todoList.length - completedCount;
-  let progressBar = document.getElementById('todo-progress');
-  progressBar.style.width = `${(completedCount / todoList.length) * 100}%`;
-  progressBar.innerHTML = `${completedCount}/${todoList.length} completed`;
-}
-
-// Mark an item as completed
-function toggleCompleted(index) {
+// Toggle the completed state of an item in the todo list
+function toggleTodo(index) {
   todoList[index].completed = !todoList[index].completed;
-  updateProgressBar();
+  saveTodoList();
+}
+
+// Save the todo list to storage
+function saveTodoList() {
+  chrome.storage.local.set({'todoList': todoList}, function() {
+    console.log('Todo list saved to storage');
+  });
 }
 
 // Render the todo list
 function renderTodoList() {
-  let todoListElement = document.getElementById('todo-list');
+  // Clear the todo list element
+  const todoListElement = document.getElementById('todo-list');
   todoListElement.innerHTML = '';
-  todoList.forEach((todo, index) => {
-    let todoElement = document.createElement('div');
+
+  // Render each item in the todo list
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+
+    // Create a list item element for the todo
+    const todoElement = document.createElement('li');
     todoElement.classList.add('todo');
     if (todo.completed) {
       todoElement.classList.add('completed');
     }
-    todoElement.innerHTML = `
-      <input type="checkbox" onclick="toggleCompleted(${index})" ${todo.completed ? 'checked' : ''}>
-      <span>${todo.text}</span>
-      <button onclick="removeTodo(${index})">x</button>
-    `;
+
+    // Create a checkbox element for the todo
+    const checkboxElement = document.createElement('input');
+    checkboxElement.type = 'checkbox';
+    checkboxElement.checked = todo.completed;
+    checkboxElement.addEventListener('click', function() {
+      toggleTodo(i);
+      renderTodoList();
+    });
+
+    // Create a span element for the todo text
+    const textElement = document.createElement('span');
+    textElement.textContent = todo.text;
+
+    // Create a button element for the remove button
+    const removeButtonElement = document.createElement('button');
+    removeButtonElement.textContent = 'Remove';
+    removeButtonElement.addEventListener('click', function() {
+      removeTodo(i);
+      renderTodoList();
+    });
+
+    // Append the checkbox, text, and remove button to the todo element
+    todoElement.appendChild(checkboxElement);
+    todoElement.appendChild(textElement);
+    todoElement.appendChild(removeButtonElement);
+
+    // Append the todo element to the todo list element
     todoListElement.appendChild(todoElement);
-  });
+  }
+
+  // Update the progress bar
+  const progressElement = document.getElementById('todo-progress');
+  progressElement.style.width = (todoList.filter(function(todo) {
+    return todo.completed;
+  }).length / todoList.length * 100) + '%';
+  progressElement.setAttribute('data-label', todoList.filter(function(todo) {
+    return todo.completed;
+  }).length + '/' + todoList.length);
 }
 
-// Add a new todo item when the form is submitted
-let form = document.getElementById('todo-form');
-form.addEventListener('submit', event => {
+// Add a new item to the todo list when the form is submitted
+const formElement = document.getElementById('todo-form');
+formElement.addEventListener('submit', function(event) {
   event.preventDefault();
-  let input = document.getElementById('todo-input');
-  addTodo(input.value);
-  input.value = '';
-  renderTodoList();
+
+  // Get the todo text from the form input
+  const inputElement = document.getElementById('todo-input');
+  const text = inputElement.value.trim();
+  if (text !== '') {
+    // Add the todo to the list and clear the input
+    addTodo(inputElement.value);
+    inputElement.value = '';
+    renderTodoList();
+  }
 });
 
-// Initial render of the todo list
-renderTodoList();
+chrome.storage.local.get(['todoList'], function(result) {
+  if (result.todoList) {
+  todoList = result.todoList;
+  } else {
+  chrome.storage.local.set({'todoList': []});
+  }
+  renderTodoList();
+});
